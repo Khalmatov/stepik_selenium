0b0950588c3ad09e272e6283602b42fa066d7f13
Aladdin <aladdin@hp>
2021-07-24 22:18:57 +0300

    change test_main_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,8 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser: WebDriver, url: str):
         self.browser = browser
         self.url = url
 
