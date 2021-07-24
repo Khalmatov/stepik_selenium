cb6da3f50dc48a37091bb4c531c81e49a30e587c
Aladdin <aladdin@hp>
2021-07-24 22:31:45 +0300

    add method should_be_login_link

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,18 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
-
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage:
-    def __init__(self, browser: WebDriver, url: str):
+    def __init__(self, browser: WebDriver, url: str, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
